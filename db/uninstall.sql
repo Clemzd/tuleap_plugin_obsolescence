ab564839f977e76fb0f9a6b790d67d52d79e32a1
Louspirit <guillaume.escarieux@free.fr>
2014-06-12 20:15:34 +0100

    Mise à jour du uninstall.sql pour la désinstallation du service obsolescence (non testé) Suppression de libellés inutilisés

--- db/uninstall.sql
+++ db/uninstall.sql
@@ -6,3 +6,6 @@
 DROP TABLE `plugin_obsolescence_technologies`;
 DROP TABLE `plugin_obsolescence_groups_technologies`;
 
+
+-- Delete of the service for the plugin Obsolescence
+DELETE FROM 'service' WHERE 'short_name'='obsolescence';
