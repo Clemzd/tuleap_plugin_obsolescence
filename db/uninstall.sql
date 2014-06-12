17b1f344747ed7ccdf3a662649635a047f0259fb
Louspirit <guillaume.escarieux@free.fr>
2014-06-12 22:02:57 +0100

    Correction install et uninstall scripts

--- db/uninstall.sql
+++ db/uninstall.sql
@@ -2,10 +2,8 @@
 ## Sql Uninstall Script
 ##
 -- DROP TABLE plugin_obsolescence_my_table;
-
-DROP TABLE `plugin_obsolescence_technologies`;
-DROP TABLE `plugin_obsolescence_groups_technologies`;
-
+DROP TABLE plugin_obsolescence_groups_technologies;
+DROP TABLE plugin_obsolescence_technologies;
 
 -- Delete of the service for the plugin Obsolescence
-DELETE FROM `service` WHERE `short_name`=`obsolescence`;
+DELETE FROM service WHERE short_name="obsolescence";
